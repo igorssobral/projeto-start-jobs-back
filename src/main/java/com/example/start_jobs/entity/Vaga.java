e087fc636925c0d278b0dcb7095520670410a46f
igorssobral <igorgmz1421@gmail.com>
2025-02-06 21:37:54 -0300

    refactor: refatorando entidades

--- src/main/java/com/example/start_jobs/entity/Vaga.java
+++ src/main/java/com/example/start_jobs/entity/Vaga.java
@@ -1,11 +1,18 @@
 package com.example.start_jobs.entity;
 
+import com.example.start_jobs.dto.StatusCandidaturaDTO;
+import com.example.start_jobs.dto.VagaDTO;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "Vaga")
 public class Vaga {
@@ -17,9 +24,27 @@ public class Vaga {
     private String descricao;
     private String empresa;
     private String localizacao;
+    private String senioridade;
+    private String modeloTrabalho;
+    @OneToMany(mappedBy = "vaga")
+    private List<Candidatura> candidaturas;
+    @Column(name = "data_criacao")
+    private LocalDate dataCriacao;
 
-    @Column(name = "data_criacao", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime dataCriacao;
+    private String url;
 
-    private String imagem;
+    public Vaga(VagaDTO vaga) {
+        this.idVaga = vaga.getId();
+        this.titulo = vaga.getTitulo();
+        this.descricao = vaga.getDescricao();
+        this.empresa = vaga.getEmpresa();
+        this.localizacao = vaga.getLocalizacao();
+        this.senioridade = vaga.getSenioridade();
+        this.modeloTrabalho = vaga.getModeloTrabalho();
+        this.candidaturas = vaga.getCandidaturas().stream()
+                .map(Candidatura::new)
+                .collect(Collectors.toList());
+        this.dataCriacao = vaga.getDataCriacao();
+        this.url = vaga.getUrl();
+    }
 }
