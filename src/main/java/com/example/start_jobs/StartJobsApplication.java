8f5093febbd4e7e49300e31bd0be9f62ff303b1e
igorssobral <igorgmz1421@gmail.com>
2025-03-07 23:31:26 -0300

    .

--- src/main/java/com/example/start_jobs/StartJobsApplication.java
+++ src/main/java/com/example/start_jobs/StartJobsApplication.java
@@ -16,6 +16,7 @@ public class StartJobsApplication  implements WebMvcConfigurer, CommandLineRunne
 
 		Dotenv dotenv = Dotenv.configure().load();
 
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
